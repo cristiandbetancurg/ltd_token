4a560d69377d7efbb456f1774157a12604be12f2
Cristian Betancur <34930340+cristiandbetancurg@users.noreply.github.com>
2024-05-26 19:05:39 -0400

    Create token.sol

---
+++ token.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+// LTD - ShibDream
+
+pragma solidity 0.8.23;
+
+import "@openzeppelin/contracts/utils/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract Token is ERC20Burnable, ERC20Capped, Pausable, Ownable {
+
+
+
+
+
+}
